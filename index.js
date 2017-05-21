ebef4fc65770f05d2de28a60ad05b5a35d0eab22
Bogdan Raducan <bogdan.raducan@emag.ro>
2017-05-21 13:43:04 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express =require('express');
+var app = express();
+var port=process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.get('/',function(req,res){
+    console.log('hello from server');
+});
+
+app.listen(port);
+
+console.log('Server Listening at port '+port);
